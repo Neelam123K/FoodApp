00608f381e8644b6329279bc4240e0a56371cba5
Shubham Kashyap <shubhamkashyap107@gmail.com>
2025-01-13 16:08:55 +0530

    Commit

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import {Routes, Route} from "react-router-dom"
+import Landing from './Components/Landing'
+import AllRestaurants from './Components/AllRestaurants'
+import Menu from './Components/Menu'
+import CarouselRestaurants from './Components/CarouselRestaurants'
+import Search from './Components/Search'
+import Checkout from './Components/Checkout'
+
+const App = () => {
+  return (
+    <div>
+      
+      <Routes>
+        <Route path='/' element={<Landing />} />
+        <Route path='/restaurants' element={<AllRestaurants />} />
+        <Route path='/menu/:id' element={<Menu />} />
+        <Route path='/carrestaurants/:id' element={<CarouselRestaurants  />} />
+        <Route path='/search' element={<Search  />} />
+        <Route path='/checkout' element={<Checkout  />} />
+      </Routes>
+
+    </div>
+  )
+}
+
+export default App
